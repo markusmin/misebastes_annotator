51eb9fc7c216a4ecdd5560f02919f72050e5b52d
markusmin <40371676+markusmin@users.noreply.github.com>
2021-05-13 01:14:04 -0700

    Fixed bugs
    Fixed duplicate row bug and issues when taxonomy not annotated to species level

--- Scripts/misebastes_annotator.py
+++ Scripts/misebastes_annotator.py
@@ -12,7 +12,9 @@ import Bio
 from Bio import SeqIO
 
 def combined_script(fasta_file, anacapa_file):
-    # Create our hash table to add the sequences
+    # Create our hash table to add the sequences (empty dictionary)
+    # Here we are setting up a dictionary where the sequences act as the keys
+    # and the taxonomy acts as the values
     sequences={}
     infile = anacapa_file
     capa = open(infile,'r')
@@ -23,20 +25,49 @@ def combined_script(fasta_file, anacapa_file):
         sequence = str(seq_record.seq).upper()
         # If the sequence isn't yet in the dictionary, the sequence and its ID
         # will be added
+        #if sequence not in sequences:
         if sequence not in sequences:
         	sequences[sequence] = seq_record.id.replace("S.","Sebastes ")
+        # Don't add the same species twice
+        # elif seq_record.id in sequences:
+        # 		sequences[sequence] = sequences[sequence]
     	# If it is already in the hash table, we're just gonna concatenate the ID
-    	# of the current sequence to another one that is already in the hash table
+    	# of the current sequence to another one that is already in the hash table.
+    	# But don't concatenate add the same species name twice
         else:
         	sequences[sequence] += ", " + seq_record.id.replace("S.","Sebastes ")
+        	
+    # Create a new version of the sequence dictionary that doesn't have any repeated taxa;
+    # use this version of the dictionary later to ensure that no extra lines are added
+    temp = []
+    unique_taxa = dict()
+    for key, val in sequences.items():
+        if val not in temp:
+            temp.append(val)
+            unique_taxa[key] = val
 
     # Write the sequences
 
+	# Write one version with all of the unique barcodes; save for reference
     # Create a file in the same directory where you ran this script
     with open("trimmed_" + fasta_file, "w+") as output_file:
         # Just read the hash table and write on the file as a fasta format
         for sequence in sequences:
             output_file.write(">" + sequences[sequence] + "\n" + sequence + "\n")
+    
+    # Write one version with only the unique species combinations; this isn't necessary,
+    # but could be used to keep a record
+    # Create a file in the same directory where you ran this script
+    #with open("trimmed_" + fasta_file, "w+") as output_file:
+        # Just read the hash table and write on the file as a fasta format
+        # Create a list of values to store taxa you have already written
+        #writtentaxa = []
+        #for sequence in sequences:
+        	# If we've already written it, don't write it again
+            #if sequences[sequence] not in writtentaxa:
+                #output_file.write(">" + sequences[sequence] + "\n" + sequence + "\n")
+            # Add to the list to make sure we don't write it twice
+            #writtentaxa.append(sequences[sequence])
 
     #print("CLEAN!!!\nPlease check trimmed_" + fasta_file)
 
@@ -51,18 +82,20 @@ def combined_script(fasta_file, anacapa_file):
         line2=line.strip('\n')
 		# Split the line into elements by tab
         Element=line2.split('\t')
-		#print(Element[0])
         Element1 = Element[0]
         if Element[0] == "sum.taxonomy":
         	print('%s\t%s\t%s' % (Element[0],'matching.species', '\t'.join(Element[1:])))
         else:
         	taxa=Element1.split(";")
-        	if len(taxa) > 6:
-        #matching_species = sequences[sequence[(taxa[6])]]
-        		for key in sequences.keys():
-        			if str(taxa[6]) in str(sequences[key]):
-        				newColumn = str(sequences[key])
+        	if len(taxa) > 6 and len(str(taxa[6]))>1:
+        # Look for matching species
+        		for key in unique_taxa.keys():
+        			if str(taxa[6]) in str(unique_taxa[key]):
+        				newColumn = str(unique_taxa[key])
         				print('%s\t%s\t%s' % (Element[0], newColumn, '\t'.join(Element[1:])))
+        	else:
+        		print('%s\t%s\t%s' % (Element[0], "NA", '\t'.join(Element[1:])))
+        		
 
 userParameters = sys.argv[1:]
 
@@ -72,7 +105,7 @@ try:
     else:
         print("Please ensure that you entered the right number of arguments!")
 except:
-    print("There is a problem! You really messed up somewhere...")
+    print("Error. Please consult the Vignette.")
 
 
 
